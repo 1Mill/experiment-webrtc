ff66bf84b7a44313a3d9475820c064c691731458
1Mill <erik.ekberg@outlook.com>
2021-02-27 19:06:26 -0800

    Scaffold agora.io integration - part 2

--- projects/getting-started/index.js
+++ projects/getting-started/index.js
@@ -1,9 +1,16 @@
-navigator.mediaDevices.getUserMedia({
-	audio: true,
-	video: true,
-}).then(res => {
-	console.log(res)
-}).catch(err => {
-	console.error(err)
-	alert(`getUserMedia() error: ${err.name}`)
-})
+var rtc = {
+	client: null,
+	localAudioTrack: null,
+}
+var options = {
+	appId: window.$_projectEnvironment.appId,
+	channeL: 'demo_channel_name',
+	token: null,
+}
+
+async function startBasicCall() {
+	console.log('TESTING')
+	return true
+}
+
+startBasicCall().then(res => console.log(res)).catch(err => console.error(err))
