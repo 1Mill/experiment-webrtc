90ce15792940bc546cfdd65a980ebfa667694991
1Mill <erik.ekberg@outlook.com>
2021-02-28 15:25:36 -0800

    Update index.js

--- projects/getting-started-video/index.js
+++ projects/getting-started-video/index.js
@@ -12,7 +12,24 @@ var options = {
 
 async function startBasicCall() {
 	try {
-		console.log('Hello world!')
+		// * Create RTC client
+		rtc.client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' })
+
+		// * Join a channel
+		const uid = await rtc.client.join(options.appId, options.channel, options.token, null)
+		console.log('Your user ID is: ', uid)
+
+		// * Create an audio track linked to the user's microphone
+		rtc.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack()
+		// * Create a video track linked to the user's camera
+		rtc.localVideoTrack = await AgoraRTC.createCameraVideoTrack()
+
+		// * Publish the audio and video tracks to the joined channel
+		await rtc.client.publish([
+			rtc.localAudioTrack,
+			rtc.localVideoTrack,
+		])
+		console.log('Audo and video tracks successfully published!')
 	} catch (err) {
 		console.error(err)
 	}
