2f2b1d339672069d8f0130a61b18e46bfa6d97d7
1Mill <erik.ekberg@outlook.com>
2021-02-27 22:50:21 -0800

    Update index.js

--- projects/getting-started/index.js
+++ projects/getting-started/index.js
@@ -10,13 +10,18 @@ var options = {
 }
 
 async function startBasicCall() {
-	console.log('TESTING')
+	// * Initialize
+	rtc.client = AgoraRTC.createClient({ codec: 'vp8', mode: 'rtc' })
 
-	rtc.client = AgoraRTC.createClient({ mode: 'live', codec: 'vp8' })
-	var uid
-	rtc.client.join(options.appId, options.channel, options.token, null)
-		.then(res => uid = res)
-		.catch(err => console.error(err))
+	// * Generate ID
+	var uid = await rtc.client.join(options.appId, options.channel, options.token, null)
+
+	// * Access microphone
+	rtc.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack()
+	// * Publish channel
+	await rtc.client.publish([rtc.localAudioTrack])
+
+	console.log('Success!')
 
 	return true
 }
