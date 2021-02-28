fa03616ee3019f4a6f979c4a55e4386a887c8a91
1Mill <erik.ekberg@outlook.com>
2021-02-28 15:20:13 -0800

    Scaffold video + remove boilerplate

--- projects/getting-started-video/index.js
+++ projects/getting-started-video/index.js
@@ -1,7 +1,8 @@
-// * https://docs.agora.io/en/Voice/start_call_audio_web_ng?platform=Web#method-2-through-the-cdn
+// * https://docs.agora.io/en/Video/start_call_web_ng?platform=Web#basic-process
 var rtc = {
 	client: null,
 	localAudioTrack: null,
+	localVideoTrack: null,
 }
 var options = {
 	appId: window.$_projectEnvironment.appId,
@@ -11,33 +12,7 @@ var options = {
 
 async function startBasicCall() {
 	try {
-		// * Initialize
-		rtc.client = AgoraRTC.createClient({ codec: 'vp8', mode: 'rtc' })
-
-		// * Generate ID
-		var uid = await rtc.client.join(options.appId, options.channel, options.token, null)
-		console.log('Your ID is: ', uid)
-
-		// * Create audio object with access to the microphone
-		rtc.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack()
-		// * Publish and pass the local audit object / track for other to access
-		await rtc.client.publish([rtc.localAudioTrack])
-
-		console.log('Success!')
-
-		rtc.client.on('user-published', async function(user, mediaType) {
-			await rtc.client.subscribe(user, mediaType)
-			console.log('Subscribe success')
-
-			if (mediaType === 'audio') {
-				var remoteAudioTrack = user.audioTrack
-				remoteAudioTrack.play()
-			}
-		})
-
-		rtc.client.on('user-unpublished', function(user) {
-			console.log('User left the room: ', user)
-		})
+		console.log('Hello world!')
 	} catch (err) {
 		console.error(err)
 	}
@@ -46,6 +21,8 @@ async function startBasicCall() {
 async function leaveCall() {
 	try {
 		rtc.localAudioTrack.close()
+		rtc.localVideoTrack.close()
+
 		await rtc.client.leave()
 	} catch (err) {
 		console.error(err)
